64e75dc26e957533986119a24f5a61c08ad9b2e7
Muhammad Umair <93200938+umair763@users.noreply.github.com>
2024-12-26 16:16:28 +0500

    Update

--- static/script.js
+++ static/script.js
@@ -1,102 +1,108 @@
-const wrapperAdjustment = document.getElementById('wrapperAD');
-wrapperAdjustment.classList.remove('wrapperAdjust');
+const wrapperAdjustment = document.getElementById("wrapperAD");
+wrapperAdjustment.classList.remove("wrapperAdjust");
 
 // Function to trigger file input when the container is clicked
 function triggerFileInput() {
-   document.getElementById('fileInput').click();
+	document.getElementById("fileInput").click();
 }
 
 // Handle file input selection
 function handleImageSelection(event) {
-   const file = event.target.files[0];
-   if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-         const imageUrl = e.target.result;
-         document.getElementById('uploadedImage').src = imageUrl;
-      };
-      reader.readAsDataURL(file);
-   }
+	const file = event.target.files[0];
+	if (file) {
+		const reader = new FileReader();
+		reader.onload = function (e) {
+			const imageUrl = e.target.result;
+			document.getElementById("uploadedImage").src = imageUrl;
+		};
+		reader.readAsDataURL(file);
+	}
 }
 
 // Handle image drop
 function allowDrop(event) {
-   event.preventDefault();
+	event.preventDefault();
 }
 
 function handleDrop(event) {
-   event.preventDefault();
-   const file = event.dataTransfer.files[0];
-   if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-         const imageUrl = e.target.result;
-         document.getElementById('uploadedImage').src = imageUrl;
-      };
-      reader.readAsDataURL(file);
-   }
+	event.preventDefault();
+	const file = event.dataTransfer.files[0];
+	if (file) {
+		const reader = new FileReader();
+		reader.onload = function (e) {
+			const imageUrl = e.target.result;
+			document.getElementById("uploadedImage").src = imageUrl;
+		};
+		reader.readAsDataURL(file);
+	}
 }
 
 // Function to classify the uploaded image
 function classifyImage() {
-   // Hide image guidelines
-   document.getElementById('guidelines').classList.add('guidelines-hidden');
+	// Hide image guidelines
+	document.getElementById("guidelines").classList.add("guidelines-hidden");
 
-   // Show loading bar
-   document.getElementById('loadingBar').style.display = 'block';
+	// Show loading bar
+	document.getElementById("loadingBar").style.display = "block";
 
-   const fileInput = document.getElementById('fileInput');
-   const file = fileInput.files[0];
-   if (!file) {
-      alert('Please upload an image.');
-      return;
-   }
+	const fileInput = document.getElementById("fileInput");
+	const file = fileInput.files[0];
+	if (!file) {
+		alert("Please upload an image.");
+		return;
+	}
 
-   // Create FormData and append the file
-   const formData = new FormData();
-   formData.append('image', file);
+	// Create FormData and append the file
+	const formData = new FormData();
+	formData.append("image", file);
 
-   // Send image to the backend
-   fetch('/predict', {
-      method: 'POST',
-      body: formData,
-   })
-      .then((response) => response.json())
-      .then((data) => {
-         document.getElementById('loadingBar').style.display = 'none';
+	// Send image to the backend
+	fetch("/predict", {
+		method: "POST",
+		body: formData,
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			setTimeout(() => {
+				document.getElementById("loadingBar").style.display = "none";
+			}, 2000);
 
-         if (data.error) {
-            alert('Error: ' + data.error);
-         } else {
-            document.getElementById('prediction').textContent = `${data.prediction} `;
-            document.getElementById('confidence').textContent = `${data.confidence}`;
-            document.getElementById('result').classList.add('visible');
-         }
-      })
-      .catch((error) => {
-         document.getElementById('loadingBar').style.display = 'none';
-         console.error('Error:', error);
-         alert('Failed to classify the image. Please try again.');
-      });
+			if (data.error) {
+				alert("Error: " + data.error);
+			} else {
+				setTimeout(() => {
+					document.getElementById("prediction").textContent = `${data.prediction} `;
+					document.getElementById("confidence").textContent = `${data.confidence}`;
+					document.getElementById("result").classList.add("visible");
+				}, 2000);
+			}
+		})
+		.catch((error) => {
+			setTimeout(() => {
+				document.getElementById("loadingBar").style.display = "none";
+			}, 2000);
+			console.error("Error:", error);
+			alert("Failed to classify the image. Please try again.");
+		});
 }
 
 // Modify the toggleDetails function
 function toggleDetails() {
-   const slidingDetails = document.getElementById('slidingDetails');
-   const wrapperAdjustment = document.getElementById('wrapperAD');
+	const slidingDetails = document.getElementById("slidingDetails");
+	const wrapperAdjustment = document.getElementById("wrapperAD");
 
-   // Toggle the 'open' class to slide in or out the sliding details
-   if (slidingDetails.classList.contains('open')) {
-      slidingDetails.classList.remove('open');
-      // Hide the sliding details by setting display to 'none' when it's closed
-      slidingDetails.style.display = 'none';
-      // Remove the 'wrapperAdjust' class when sliding details are closed
-      wrapperAdjustment.classList.remove('wrapperAdjust');
-   } else {
-      slidingDetails.classList.add('open');
-      // Display the sliding details when it's opened
-      slidingDetails.style.display = 'block';
-      // Add the 'wrapperAdjust' class when sliding details are opened
-      wrapperAdjustment.classList.add('wrapperAdjust');
-   }
+	// Toggle the 'open' class to slide in or out the sliding details
+	if (slidingDetails.classList.contains("open")) {
+		slidingDetails.classList.remove("open");
+		// Hide the sliding details by setting display to 'none' when it's closed
+		slidingDetails.style.display = "none";
+		// Remove the 'wrapperAdjust' class when sliding details are closed
+		wrapperAdjustment.classList.remove("wrapperAdjust");
+	} else {
+		slidingDetails.classList.add("open");
+		// Display the sliding details when it's opened
+		slidingDetails.style.display = "block";
+		// Add the 'wrapperAdjust' class when sliding details are opened
+		wrapperAdjustment.classList.add("wrapperAdjust");
+	}
 }
